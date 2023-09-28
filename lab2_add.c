25b613324f5a670b3835afc3643ff55fd8f6cdc1
tanvikad <tanvika.dasari@gmail.com>
2023-09-27 20:45:31 -0700

    p1a correct

--- lab2_add.c
+++ lab2_add.c
@@ -1,35 +1,117 @@
 #include <pthread.h>
 #include <stdio.h> //for printing
+#include <stdlib.h> 
+#include <time.h>
 
-// void add(long long *pointer, long long value) {
-//     long long sum = *pointer + value;
-//     *pointer = sum;
-// }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h> 
+#include <errno.h>
+#include <string.h>
+#include <poll.h>
+#include <termios.h>
+#include <sys/wait.h>
+#include <sys/types.h> 
+#include <netinet/in.h>
+#include <netdb.h> 
+
 
 
 struct addArguments {
     long long *pointer;
-    long long value;
+    long long iterations;
     int tid; 
 };
 
-void add(struct addArguments* args) {
-    long long sum = *(args->pointer) + 1;
-    *args->pointer = sum;
-    printf("The thread is %d \n", args->tid);
+void add_value(long long* pointer, long long value) {
+    long long sum = *pointer + value;
+    *pointer = sum;
 }
 
-int main() {
-    int num_threads = 10;
-    pthread_t threads[num_threads];
-    struct addArguments args[num_threads];
+void add(struct addArguments* args) {
+    for(int i = 0; i < args->iterations; i++) {
+        add_value(args->pointer, 1);
+    }
+
+    for(int i = 0; i < args->iterations; i++) {
+        add_value(args->pointer, -1); 
+    }
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+
+    int curr_option;
+    const struct option options[] = {
+        { "threads",  required_argument, NULL,  't' },
+        { "iterations", required_argument, NULL,  'i' },
+        { 0, 0, 0, 0}
+    };
+
+    int num_threads = 1;
+    int num_iterations = 1;
+
+    while((curr_option = getopt_long(argc, argv, "i:t", options, NULL)) != -1)  {
+        switch(curr_option) {
+            case 't':
+                sscanf(optarg, "%d", &num_threads);
+                // num_threads = optarg;
+                break;
+            case 'i':
+                sscanf(optarg, "%d", &num_iterations);
+                // num_iterations = optarg;
+                break;
+            default:
+                fprintf(stderr, "Use the options --iterations --threads");
+                exit(1);
+                break;
+        }
+    }
+
+    printf("The number of threads is %d \n", num_threads);
+    printf("The number of interations is %d \n", num_iterations);
+
+    // num_threads = 10;
+    pthread_t *threads = malloc(sizeof(pthread_t) * num_threads);
+    struct addArguments a; 
+    struct addArguments *args = malloc(sizeof(a) * num_threads);
+    // struct addArguments args[10];
     long long pointer_value = 0;
+
+    struct timespec start, stop;
+    
+    if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) {
+        fprintf(stderr, "Clock Get Time Fails %s\n", strerror(errno));
+        exit(1);
+    }
 
     for (int i = 0; i < num_threads; i++) {
         args[i].pointer = &pointer_value;
         args[i].tid = i;
+        args[i].iterations = num_iterations;
         int rc = pthread_create(&threads[i], NULL, &add, &args[i]);
     }
 
-    printf("The pointer_value is %lld", pointer_value);
-}
+
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) {
+        fprintf(stderr, "Clock Get Time Fails %s\n", strerror(errno));
+        exit(1);
+    }
+
+    double accum = (stop.tv_sec - start.tv_sec) * 1000000000
+          + (stop.tv_nsec - start.tv_nsec);
+
+    int num_operations = 2 * num_threads * num_iterations;
+    printf("add-none, %d, %d, %d, %f, %lld\n", num_threads, num_iterations, num_operations, accum/num_operations, pointer_value);
+
+    free(threads);
+    free(args);
+}  
